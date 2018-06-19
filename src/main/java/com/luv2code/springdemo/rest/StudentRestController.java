b3e26c20ef2705a68df64c93c60364f0a7445957
Martin Prucha <prucha.martin@gmail.com>
2018-06-19 08:02:02 +0200

    Lecture 495 completed

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,21 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define endpoint fo "/students" - return list of students
-	@GetMapping("/students")
-	public List<Student> getStudents() {
-		List<Student> theStudents = new ArrayList<>();
+	private List<Student> theStudents;
+	
+	// define @PostConstruct annotation to load the student data .. only once!
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Marry", "Smith"));
-				
+		theStudents.add(new Student("Marry", "Smith"));		
+	}
+	
+	// define endpoint fo "/students" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 		return theStudents;
 	}
 }
